2fff79215d61e7feefa5c3819285ab357ad958c2
Rene <renegarcia80@gmail.com>
2022-06-19 23:07:19 -0700

    ready to deploy

--- config/database.js
+++ config/database.js
@@ -3,10 +3,10 @@ module.exports = ({ env }) => ({
     client: 'mysql',
     connection: {
       host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 3307),
+      port: env.int('DATABASE_PORT', 3306),
       database: env('DATABASE_NAME', 'strapi-learn-tagalog'),
       user: env('DATABASE_USERNAME', 'strapi-learn-tagalog'),
-      password: env('DATABASE_PASSWORD', 'MkoNji8900^'),
+      password: env('DATABASE_PASSWORD', 'MkoNji890^'),
       ssl: env.bool('DATABASE_SSL', false),
     },
   },
--- src/index.js
+++ src/index.js
@@ -7,7 +7,45 @@ module.exports = {
    *
    * This gives you an opportunity to extend code.
    */
-  register(/*{ strapi }*/) {},
+  register({ strapi }) {
+    const extensionService = strapi.plugin('graphql').service('extension');
+    const extension = ({ nexus }) => ({
+      // GraphQL SDL
+
+      types: [
+        nexus.objectType({
+          name: "Rene",
+          definition(t) {
+            t.int('age')
+          }
+        }),
+        nexus.extendType({
+          type: "Query",
+          definition(t) {
+            t.field('joe', {
+              type: "Rene", resolve() {
+                console.log("yoyoyo");
+                return {age: 5}
+              }
+            })
+          }
+        }),
+
+      ],
+
+      resolversConfig: {
+        "Query.joe": {
+          auth: false
+        },
+      },
+
+
+
+    })
+
+    extensionService.use(extension);
+
+  },
 
   /**
    * An asynchronous bootstrap function that runs before
